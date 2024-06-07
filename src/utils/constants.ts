23b62dcb7c4bccae055732db0180bf6d2111cd64
Noman <noman@noman.land>
2024-06-07 14:43:37 -0400

    Remove unused variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,5 @@
 export const HS_AUTH_URL = 'https://app.hubspot.com/oauth/authorize';
 export const HS_OAUTH_TOKEN_URL = 'https://api.hubapi.com/oauth/v1/token';
-export const HS_CONTACTS_URL =
-  'https://api.hubapi.com/contacts/v1/lists/all/contacts/all?count=1';
 
 export const SCOPES = ['crm.objects.contacts.read', 'conversations.read'].join(
   ' '
