385d37336c12cab3dccad9d2115d9310f6a87735
Noman <noman@noman.land>
2024-09-05 11:21:57 -0400

    Add more scopes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,11 @@
 export const HS_AUTH_URL = 'https://app.hubspot.com/oauth/authorize';
 export const HS_OAUTH_TOKEN_URL = 'https://api.hubapi.com/oauth/v1/token';
-export const SCOPES = ['crm.objects.contacts.read', 'conversations.read'].join(
-  ' '
-);
+export const SCOPES = [
+  'crm.objects.contacts.read',
+  'conversations.read',
+  'crm.objects.appointments.read',
+  'crm.objects.services.read',
+  'crm.objects.courses.read',
+  'crm.objects.listings.read',
+].join(' ');
 export const basePath = '/oauth/v1/apps/chat';
